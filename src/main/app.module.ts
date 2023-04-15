eea7f0be36164473adfb74929767d37aa0383447
granadalook <tiven17.jsga@gmail.com>
2023-04-14 22:40:06 -0500

    ADD/ Se agregan dependencias de TypeOrm   se crea modulo global de conexion a base de datos se agregan variables de entorno

--- src/main/app.module.ts
+++ src/main/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { environments } from '../environments';
+import config from '../config/config';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environments[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+      load: [config],
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
