84291bfd7b937a622f5b22dbd0c20ecd5f0827e4
granadalook <tiven17.jsga@gmail.com>
2023-04-14 22:18:56 -0500

    ADD/Se  configura el proyecto se instalan dependencias y se agrega swagger  como documetacion  de la app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './main/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('SOCIAL WIRE APP')
+    .setDescription(
+      'Social Wires es una red social que busca facilitar en un espacio publicaciones de personas con el fin de expresar lo que gusten en la red',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc/swagger', app, document);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
--- src/main/app.controller.ts
+++ src/main/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+@ApiTags('MAIN MODULE')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'HELLO WORD IN NESTJS' })
   getHello(): string {
     return this.appService.getHello();
   }
